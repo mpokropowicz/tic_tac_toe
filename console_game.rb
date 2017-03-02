8f87d1a92731bd7d62e74337e3a09b8003fd65bd
Max Pokropowicz <Max Pokropowicz>
2017-03-01 22:17:18 -0500

    test 3 passes

--- console_game.rb
+++ console_game.rb
@@ -2,13 +2,26 @@ class ConsoleGame
 
 	attr_accessor :board
 
-	def initialize(board)
+	def initialize(board, p1, p2)
 
 		@board = board
+		@p1 = p1
+		@p2 = p2
+		@current_player = @p1
 	end
 
 	def greet_text
 
 		"Welcome to Tic Tac Toe!"
+	end
+
+	def change_player
+
+		 @current_player == @p1 ? @current_player = @p2 :  @current_player = @p1
+	end
+
+	def to_s
+
+		"The current player is #{@current_player}"
 	end
 end
--- console_game_test.rb
+++ console_game_test.rb
@@ -5,15 +5,22 @@ class TestConsoleGame < Minitest::Test
 
 	def test_start_message
 
-		game = ConsoleGame.new([""])
+		game = ConsoleGame.new([""], "", "")
 
 		assert_equal("Welcome to Tic Tac Toe!", game.greet_text)
 	end
 
 	def test_board_array
 
-		game = ConsoleGame.new(["","","", "","","", "","",""])
+		game = ConsoleGame.new(["","","", "","","", "","",""], "", "")
 
 		assert_equal(["","","", "","","", "","",""], game.board)
+	end
+
+	def test_switch_players
+
+		game = ConsoleGame.new([], "p1", "p2")
+
+		assert_equal("p2", game.change_player)
 	end
 end
