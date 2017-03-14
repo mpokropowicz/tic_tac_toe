3f77fb348446eb649bac88ec35aace666b8239cb
Max Pokropowicz <Max Pokropowicz>
2017-03-14 16:28:56 -0400

    start of web app

---
+++ app.rb
@@ -0,0 +1,10 @@
+require "sinatra"
+require_relative "console_game.rb"
+require_relative "random_ai.rb"
+require_relative "unbeatable_ai.rb"
+require_relative "human.rb"
+
+get "/" do 
+
+	"oi"
+end
